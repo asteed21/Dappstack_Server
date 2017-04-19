689c1da08b0f9a618bc09c6f3548ae3e88caeed5
asteed21 <asteed21@gmail.com>
2017-04-19 10:12:07 -0400

    extend user model, added bcrypt module

---
+++ bin/automigrate-users.js
@@ -0,0 +1,42 @@
+var path = require('path');
+
+var app = require(path.resolve(__dirname, '../server/server'));
+var ds = app.datasources.DappstackDB;
+ds.automigrate('dappStackUsers', function(err) {
+  if (err) throw err;
+
+  var dappStackUsers = [
+    {
+      firstName: 'Austin',
+      lastName: 'Steed',
+      joinDate: new Date(),
+      role: 'user',
+      email: 'asteed21@gmail.com',
+      username: 'asteed21',
+      password: 'test',
+      favorites: ['1','2']
+    },
+    {
+      firstName: 'Charlie',
+      lastName: 'Morris',
+      joinDate: new Date(),
+      role: 'user',
+      email: 'chtmorris@gmail.com',
+      username: 'chtmorris',
+      password: 'test',
+      favorites: ['2']
+    },
+  ];
+  var count = dappStackUsers.length;
+  dappStackUsers.forEach(function(dapp) {
+    app.models.dappStackUsers.create(dapp, function(err, model) {
+      if (err) throw err;
+
+      console.log('Created:', model);
+
+      count--;
+      if (count === 0)
+        ds.disconnect();
+    });
+  });
+});
