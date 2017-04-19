f80fbe038d32431572b3d72b693aa2800121fd30
asteed21 <asteed21@gmail.com>
2017-04-18 22:45:52 -0400

    added model for dapps, test data

---
+++ bin/automigrate.js
@@ -0,0 +1,36 @@
+var path = require('path');
+
+var app = require(path.resolve(__dirname, '../server/server'));
+var ds = app.datasources.DappstackDB;
+ds.automigrate('dapps', function(err) {
+  if (err) throw err;
+
+  var dapps = [
+    {
+      name: 'Money Dapp',
+      owners: ['Austin Steed'],
+      createdAt: new Date(),
+      lastModifiedAt: new Date(),
+      aboutContent: "Wow, this is another dapp intro statement. WOWWWWW!"
+    },
+    {
+      name: 'Doc Dapp',
+      owners: ['Mary Marsh'],
+      createdAt: new Date(),
+      lastModifiedAt: new Date(),
+      aboutContent: 'This is an intro statement about the dapp, it will be limited to some amount of something'
+    },
+  ];
+  var count = dapps.length;
+  dapps.forEach(function(dapp) {
+    app.models.dapps.create(dapp, function(err, model) {
+      if (err) throw err;
+
+      console.log('Created:', model);
+
+      count--;
+      if (count === 0)
+        ds.disconnect();
+    });
+  });
+});
