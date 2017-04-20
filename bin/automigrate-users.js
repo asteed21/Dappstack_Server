5264a38d5806c819564aa9f5f9c8c6ac626725d9
asteed21 <asteed21@gmail.com>
2017-04-20 15:19:16 -0400

    naming changes

--- bin/automigrate-users.js
+++ bin/automigrate-users.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 var app = require(path.resolve(__dirname, '../server/server'));
 var ds = app.datasources.DappstackDB;
-ds.automigrate('dappStackUsers', function(err) {
+ds.automigrate('dappStackUser', function(err) {
   if (err) throw err;
 
   var dappStackUsers = [
@@ -28,8 +28,8 @@ ds.automigrate('dappStackUsers', function(err) {
     },
   ];
   var count = dappStackUsers.length;
-  dappStackUsers.forEach(function(dapp) {
-    app.models.dappStackUsers.create(dapp, function(err, model) {
+  dappStackUsers.forEach(function(dappStackUser) {
+    app.models.dappStackUser.create(dappStackUser, function(err, model) {
       if (err) throw err;
 
       console.log('Created:', model);
