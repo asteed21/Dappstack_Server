7ebe490891f5c1804e7d5a5a7b1b8338cfc3e25f
asteed21 <asteed21@gmail.com>
2017-08-27 19:42:58 -0400

    model updates for multiple relations

--- server/server.js
+++ server/server.js
@@ -2,15 +2,17 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-var app = module.exports = loopback();
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var app = module.exports = loopback();
 
-
-//Passport configuration
+//Create an instance of Passport Configurator with the current app instance
 // var loopbackPassport = require('loopback-component-passport');
-// var PassportConfigurator = loopbackPassport.PassportConfigurator;
+// var PassportConfigurator = require('loopback-component-passport').PassportConfigurator;
 // var passportConfigurator = new PassportConfigurator(app);
 
+//get server going
 app.start = function() {
   // start the web server
   return app.listen(function() {
@@ -24,15 +26,6 @@ app.start = function() {
   });
 };
 
-// Build the providers/passport config
-var config = {};
-try {
-	config = require('../providers.json');
-} catch (err) {
-	console.trace(err);
-	process.exit(1); // fatal
-}
-
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
@@ -42,5 +35,36 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+/* BEGIN PASSPORT SETUP AND START FOR THIRD PARTY LOGIN*/
+
+// Load the provider configurations
+// var config = {};
+// try {
+//  config = require('../providers.json');
+// } catch(err) {
+//  console.error('Please configure your passport strategy in `providers.json`.');
+//  console.error('Copy `providers.json.template` to `providers.json` and replace the clientID/clientSecret values with your own.');
+//  process.exit(1);
+// }
+
+// // Initialize passport
+// passportConfigurator.init();
+
+// // Set up related models
+// passportConfigurator.setupModels({
+//  userModel: app.models.dappStackUser,
+//  userIdentityModel: app.models.userIdentity,
+//  userCredentialModel: app.models.userCredential
+// });
+
+// // Configure passport strategies for third party auth providers
+// for(var s in config) {
+//  var c = config[s];
+//  c.session = c.session !== false;
+//  passportConfigurator.configureProvider(s, c);
+// }
+
+/* END PASSPORT SETUP AND START FOR THIRD PARTY LOGIN */
 
 
